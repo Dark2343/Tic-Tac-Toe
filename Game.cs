5ece70d8a49b268b39666d9f18a9173f5ac8cae0
Dark <mohamed2003elashmawy@gmail.com>
2023-05-09 01:34:28 +0300

    edit

--- Game.cs
+++ Game.cs
@@ -62,17 +62,3 @@ namespace TicTacToe
         }
     }
 }
-
-/*
-private static void Main(){
-            Form mainForm = new Form();
-            Label lblFirst = new Label();
-            mainForm.Width = 300;
-            mainForm.Height = 400; 
-            lblFirst.Text = "Hello World";
-            lblFirst.Location = new Point(150,200);
-            mainForm.Controls.Add(lblFirst);
-            Application.Run(mainForm);
-        }
-
-*/
